0fe0b34333262e2142272557894b994aaf250381
Sergio <sergio.strazzacappa@est.fi.uncoma.edu.ar>
2019-05-27 04:16:39 -0300

    Finalizando contest

--- Player.java
+++ Player.java
@@ -12,7 +12,7 @@ class Player {
         while(true) {
             g.update(in);
             //g.debug();
-            g.buildOutput();
+            g.gameLogic();
             g.output();
         }
     }
@@ -22,35 +22,51 @@ class Game {
     List<Unit> units;
     List<Building> buildings;
     int gold;
+    int currentGold;
     int income;
     List<Command> output;
+    List<Cell> map;
+    List<Position> mines;
+    int numberOfUnitsOwned;
     
     public Game() {
-        units = new ArrayList<Unit>();
-        buildings = new ArrayList<Building>();
-        gold = 0;
-        income = 0;
-        output = new ArrayList<Command>();
+        this.units = new ArrayList<Unit>();
+        this.buildings = new ArrayList<Building>();
+        this.gold = 0;
+        this.currentGold = 0;
+        this.income = 0;
+        this.output = new ArrayList<Command>();
+        this.map = new ArrayList<Cell>();
+        this.mines = new ArrayList<Position>();
+        this.numberOfUnitsOwned = 0;
     }
     
     public void init(Scanner in) {
-        int numberMineSpots = in.nextInt(); // From Wood 1
+        int numberMineSpots = in.nextInt();
 
         for(int i = 0; i < numberMineSpots; i++) {
             int x = in.nextInt();
             int y = in.nextInt();
+
+            this.mines.add(new Position(x, y));
         }
     }
     
     public void update(Scanner in) {
         // Clears data from the previous turn
-        units.clear();
-        buildings.clear();
-        output.clear();
+        this.units.clear();
+        this.buildings.clear();
+        this.output.clear();
+        this.map.clear();
+        this.gold = 0;
+        this.income = 0;
+        this.numberOfUnitsOwned = 0;
 
         // My data
         gold = in.nextInt();
-        int income = in.nextInt();
+        income = in.nextInt();
+
+        this.currentGold = gold;
 
         // My opponent data
         int opponentGold = in.nextInt();
@@ -60,6 +76,12 @@ class Game {
         for(int i = 0; i < 12; i++) {
             String line = in.next();
             //System.err.println(line);
+
+            for(int j = 0; j < 12; j++) {
+                Cell c = new Cell(j, i, line.charAt(j));
+                this.map.add(c);
+                //c.debug();
+            }
         }
 
         // The buildings
@@ -70,7 +92,7 @@ class Game {
             int buildingType = in.nextInt(); // 0 HQ
             int x = in.nextInt();
             int y = in.nextInt();
-            buildings.add(new Building(x, y, buildingType, owner));
+            this.buildings.add(new Building(x, y, buildingType, owner));
         }
 
         // The units
@@ -78,43 +100,119 @@ class Game {
 
         for(int i = 0; i < unitCount; i++) {
             int owner = in.nextInt(); // 0 mine, 1 enemy
+
+            if (owner == 0) {
+                this.numberOfUnitsOwned++;
+            }
+
             int unitId = in.nextInt();
             int level = in.nextInt();
             int x = in.nextInt();
             int y = in.nextInt();
-            units.add(new Unit(x, y, level, owner, unitId));
+            this.units.add(new Unit(x, y, level, owner, unitId));
         }
     }
     
-    public void buildOutput() {
+    public void gameLogic() {
+        buildMines();
         trainUnits();
         moveUnits();
     }
+
+    private void buildMines() {
+        List<Position> miningPositions = myTerritory();
+        if(currentGold >= 20) {
+            Position mine = isAMine(miningPositions);
+            if(mine != null) {
+                currentGold -= 20;
+                output.add(new Command(CommandType.BUILD, mine, 1, true));
+            }
+        }
+    }
+
+    private Position isAMine(List<Position> miningPositions) {
+        Position mine = null;
+        int i = 0;
+        int j = 0;
+
+        while(i < miningPositions.size() && mine == null) {
+            j = 0;
+            while(j < mines.size() && mine == null) {
+                if(miningPositions.get(i).x == mines.get(j).x && miningPositions.get(i).y == mines.get(j).y) {
+                    mine = new Position(mines.get(j).x, mines.get(j).y);
+                }
+                j++;
+            }
+            i++;
+        }
+        return mine;
+    }
     
     private void trainUnits() {
-        Position trainingPosition = findTrainingPosition();
-
-        if (gold > 10) {
-            output.add(new Command(CommandType.TRAIN, trainingPosition, 1));
+        List<Position> trainingPositions = frontier();
+        //trainingPositions.stream().forEach(p -> System.err.println("x: " + p.x + " " + "y: " + p.y));
+        if(this.numberOfUnitsOwned < 5) {
+            for(int i = 0; i < trainingPositions.size(); i++) {
+                if(currentGold >= 10 && this.income >= 0) {
+                    currentGold -= 10;
+                    output.add(new Command(CommandType.TRAIN, trainingPositions.get(i), 1, false));
+                }
+            }
         }
     }
     
-    // Move to the center
+    // Tries to expand the frontier
     private void moveUnits() {
-        Position center = new Position(5, 5);
+        List<Position> movingPositions = frontier();
 
-        units.stream().filter(u -> u.isOwned()).forEach(myUnit -> output.add(new Command(CommandType.MOVE, center, myUnit.id))
-            );
+        for(int i = 0; i < this.units.size(); i++) {
+            double minDistance = 999;
+            Position target = null;
+
+            if(this.units.get(i).isOwned()) {
+                for(int j = 0; j < movingPositions.size(); j++) {
+                    double distance = units.get(i).p.distance(movingPositions.get(j));
+                    if(distance <= minDistance) {
+                        minDistance = distance;
+                        target = new Position(movingPositions.get(j).x, movingPositions.get(j).y);
+                    }
+                }
+                output.add(new Command(CommandType.MOVE, target, units.get(i).id, false));
+                movingPositions.remove(target);
+            }
+        }
     }
     
-    // Train near the HQ for now
-    private Position findTrainingPosition() {
-        Building HQ = getHQ();
+    // Return a list of the cells in my territory
+    private List<Position> myTerritory() {
+        List positions = new ArrayList<Position>();
 
-        if(HQ.p.x == 0) {
-            return new Position(0, 1);
+        for(int i = 0; i < this.map.size(); i++) {
+            if(map.get(i).c == CellType.OWNED_ACTIVE) {
+                positions.add(map.get(i).p);
+            }
         }
-        return new Position(11, 10);
+        //System.err.println(positions.toString());
+        return positions;
+    }
+
+    // Return a list of the adyacent cells that are not mine and active and are not void
+    private List<Position> frontier() {
+        List positions = new ArrayList<Position>();
+
+        for(int i = 0; i < this.map.size(); i++) {
+            Cell c1 = map.get(i);
+            if(c1.isOwnedAndActive()) {
+                for(int j = 0; j < map.size(); j++) {
+                    Cell c2 = map.get(j);
+                    if(c1.isAdyacent(c2) && c2.c != CellType.OWNED_ACTIVE && c2.c != CellType.VOID && !positions.contains(c2.p)) {
+                        positions.add(c2.p);
+                    }
+                }
+            }
+        }
+        //System.err.println(positions.toString());
+        return positions;
     }
     
     public void output() {
@@ -140,27 +238,37 @@ class Game {
 class Command {
     CommandType t;
     Position p;
-    int idOrLevel;
+    int idOrLevelOrBuildingType;
+    boolean isBuilding;
     String msg;
     
-    public Command(CommandType t, Position p, int idOrLevel) {
+    public Command(CommandType t, Position p, int idOrLevelOrBuildingType, boolean isBuilding) {
         this.t = t;
         this.p = p;
-        this.idOrLevel = idOrLevel;
+        this.idOrLevelOrBuildingType = idOrLevelOrBuildingType;
+        this.isBuilding = isBuilding;
         this.msg = "";
     }
 
-    public Command(CommandType t, Position p, int idOrLevel, String msg) {
+    public Command(CommandType t, Position p, int idOrLevelOrBuildingType, String msg, boolean isBuilding) {
         this.t = t;
         this.p = p;
-        this.idOrLevel = idOrLevel;
+        this.idOrLevelOrBuildingType = idOrLevelOrBuildingType;
+        this.isBuilding = isBuilding;
         this.msg = msg;
-    } 
-    
+    }
+
     public String get() {
-        String command = t.toString() + " " + idOrLevel + " " + p.x + " " + p.y + ";";
+        String command = "";
+
+        if(!this.isBuilding) {
+            command = this.t.toString() + " " + this.idOrLevelOrBuildingType + " " + this.p.x + " " + this.p.y + ";";
+        }
+        else {
+            command = this.t.toString() + " " + BuildingType.get(this.idOrLevelOrBuildingType) + " " + this.p.x + " " + this.p.y + ";";
+        }
         if(!this.msg.equalsIgnoreCase("")) {
-            command += "MSG" + " " + msg;   
+            command += "MSG" + " " + this.msg;   
         }
         return command;
     }
@@ -173,6 +281,17 @@ class Position {
     public Position(int x, int y) {
         this.x = x;
         this.y = y;
+    }
+
+    public double distance(Position p) {
+        //double euclideanDistance = Math.sqrt(Math.pow(this.x + p.x, 2) + Math.pow(this.y + p.y, 2));
+        double manhattanDistance = Math.abs(this.x - p.x) + Math.abs(this.y - p.y);
+        //System.err.println("The distance between (" + this.x + ", " + this.y + ") and (" + p.x + ", " + p.y + ") is " + manhattanDistance);
+        return manhattanDistance;
+    }
+
+    public String toString() {
+        return "(" + this.x + ", " + this.y + ")";
     }
 }
 
@@ -190,11 +309,11 @@ class Unit {
     }
     
     public void debug() {
-        System.err.println("Unit of level " + level + " at " + p.x + " " + p.y + " owned by " + owner);
+        System.err.println("Unit of level " + this.level + " at " + this.p.x + " " + this.p.y + " owned by " + this.owner);
     }
     
     public boolean isOwned() {
-        return owner == 0;
+        return this.owner == 0;
     }
 }
 
@@ -210,26 +329,64 @@ class Building {
     }
     
     public void debug() {
-        System.err.println(t + " at " + p.x + " " + p.y + " owned by " + owner);
+        System.err.println(this.t + " at " + this.p.x + " " + this.p.y + " owned by " + this.owner);
     }
     
     public boolean isHQ() {
-        return t.equals(BuildingType.HQ);
+        return this.t.equals(BuildingType.HQ);
     }
     
     public boolean isOwned() {
-        return owner == 0;
+        return this.owner == 0;
+    }
+}
+
+class Cell {
+    Position p;
+    CellType c;
+
+    public Cell(int x, int y, char c) {
+        this.p = new Position(x, y);
+        this.c = CellType.get(c);
+    }
+
+    public void debug() {
+        System.err.println(this.c + " at " + this.p.x + " " + this.p.y);
+    }
+
+    public boolean isOwnedAndActive() {
+        return this.c == CellType.OWNED_ACTIVE;
+    }
+
+    public boolean isAdyacent(Cell c) {
+        return this.p.x == c.p.x && this.p.y == c.p.y + 1 ||
+            this.p.x == c.p.x && this.p.y == c.p.y - 1 ||
+            this.p.x == c.p.x - 1 && this.p.y == c.p.y - 1 ||
+            this.p.x == c.p.x - 1 && this.p.y == c.p.y ||
+            this.p.x == c.p.x - 1 && this.p.y == c.p.y + 1 ||
+            this.p.x == c.p.x + 1 && this.p.y == c.p.y - 1 ||
+            this.p.x == c.p.x + 1 && this.p.y == c.p.y ||
+            this.p.x == c.p.x + 1 && this.p.y == c.p.y + 1;
     }
 }
 
 enum BuildingType {
-    HQ;
+    HQ,
+    MINE,
+    TOWER;
     
     public static BuildingType get(int type) {
         BuildingType buildingType = null;
+
         switch(type) {
             case 0:
                 buildingType = HQ;
+                break;
+            case 1:
+                buildingType = MINE;
+                break;
+            case 2:
+                buildingType = TOWER;
                 break;
             default:
                 buildingType = null;
@@ -240,5 +397,43 @@ enum BuildingType {
 
 enum CommandType {
     MOVE,
-    TRAIN;
+    TRAIN,
+    BUILD;
+}
+
+enum CellType {
+    VOID,
+    NEUTRAL,
+    OWNED_ACTIVE,
+    OWNED_INACTIVE,
+    OPPONENT_ACTIVE,
+    OPPONENT_INACTIVE;
+
+    public static CellType get(char type) {
+        CellType cellType = null;
+
+        switch(type) {
+            case '#':
+                cellType = VOID;
+                break;
+            case '.':
+                cellType = NEUTRAL;
+                break;
+            case 'O':
+                cellType = OWNED_ACTIVE;
+                break;
+            case 'o':
+                cellType = OWNED_INACTIVE;
+                break;
+            case 'X':
+                cellType = OPPONENT_ACTIVE;
+                break;
+            case 'x':
+                cellType = OPPONENT_INACTIVE;
+                break;
+            default:
+                cellType = null;
+        }
+        return cellType;
+    }
 }
--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-* A code of ice and fire
+# A code of ice and fire
 
+## Estrategias utilizadas
 
+1. Wood 3: Entrenar unidades cuando sea posible (gold > 10) e ir ubicandolos en las
+casillas adyacentes a la frontera. Para el movimiento de las unidades, encontrar la casilla
+conquistable mas cercana para cada unidad.
 
+2. Wood 2 & Wood 1: Optimizar la estrategia anterior.
 
+3. Bronze: Construir la mina inicial lo antes posible y continuar con la estrategia anterior.
+
+## Posición
+
+Al finalizar la competencia llegue a Silver quedando aproximadamente 504/512 y en la tabla general en el puesto 851
